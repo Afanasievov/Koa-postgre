746b23e377d97b0376b558153681c08e5957fa93
Oleksandr Afanasiev <o.afanasiev@mobidev.biz>
2018-02-06 19:02:37 +0200

    Add users table and passport packages.

---
+++ src/server/auth.js
@@ -0,0 +1,23 @@
+const passport = require('koa-passport');
+const LocalStrategy = require('passport-local').Strategy;
+const knex = require('./db/connection');
+
+const options = {};
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser((id, done) => knex('users')
+  .where({ id })
+  .then(user => done(null, user))
+  .catch(err => done(err, null)));
+
+passport.use(new LocalStrategy(options, (username, password, done) => knex('users')
+  .where(username)
+  .then((user) => {
+    if (user && password === user.password) {
+      return done(null, user);
+    }
+
+    return done(null, false);
+  })
+  .catch(err => done(err))));
--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const koaLogger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
 const logger = require('./services/logger');
+const session = require('koa-session');
+const passport = require('koa-passport');
 
 const indexRoutes = require('./routes/index');
 const movieRoutes = require('./routes/movies');
@@ -9,12 +11,27 @@ const movieRoutes = require('./routes/movies');
 const app = new Koa();
 const PORT = 1337;
 
+// sessions
+app.keys = ['super-secret-key'];
+app.use(session(app));
+
+// logger
 app.use(koaLogger());
+
+// body parser
 app.use(bodyParser());
 
+// authentication
+require('./auth');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
 app.use(indexRoutes.routes());
 app.use(movieRoutes.routes());
 
+// server
 const server = app.listen(PORT, () => {
   logger.info(`Server listening on port: ${PORT}`);
 });
